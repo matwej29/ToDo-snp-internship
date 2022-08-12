ac7a1fa8e0248a2aa8c87457d16b8ba0ba9d69ca
matwej29 <51135127+matwej29@users.noreply.github.com>
2022-08-12 20:45:45 +0300

    rewrite change mechanic
    toggle button is not functioning yet as intended

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const addItem = (text) => {
   div.id = item.id;
   div.dataset.state = item.state;
   div.dataset.clicked = false;
-  // div.innerHTML = `f
+  // div.innerHTML = `
   // <input type="checkbox" onclick="changeState(${id}, this.value)"/>
   // <p>${text}</p>
   // <button onclick="deleteItem(${id})">X</button>`;
@@ -30,32 +30,56 @@ const addItem = (text) => {
   content.textContent = item.text;
   content.classList = "task-content";
 
-  div.ondblclick = () => {
-    div.classList.add("editing");
-    content.contentEditable = true;
-
-    content.addEventListener("keydown", (event) => {
-      if (event.code == "Escape" || event.code == "Enter") {
-        content.contentEditable = false;
-        div.classList.remove("editing");
-        const ind = list.findIndex((item) => item.id == div.id);
-        const newValue = document.querySelector(`div[id="${div.id}"] p`);
-
-        list[ind].text = newValue;
-      }
-    });
-  };
-
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "X";
   deleteButton.onclick = () => {
     deleteItem(div.id);
   };
 
+  content.ondblclick = () => {
+    const edit_input = document.createElement("input");
+    edit_input.type = 'text'
+    edit_input.value = content.textContent;
+
+    div.classList.add("editing");
+    deleteButton.classList.add("hide");
+    checkbox.classList.add("hide");
+    content.classList.add("hide");
+
+    edit_input.onblur = edit_input.onchange = () => {
+      div.classList.remove("editing");
+      deleteButton.classList.remove("hide");
+      checkbox.classList.remove("hide");
+      content.classList.remove("hide");
+
+      const ind = list.findIndex((item) => item.id == div.id);
+      content.textContent = edit_input.value;
+      list[ind].text = edit_input.value; //newValue;
+      edit_input.remove();
+    };
+    content.after(edit_input);
+    edit_input.focus();
+  };
+
   div.append(checkbox, content, deleteButton);
   document.getElementsByClassName("todos")[0].append(div);
-
   update();
+
+  div.toggleComplete = () => {
+    const curr_item_ind = list.findIndex((elem) => elem.id == div.id);
+    console.log(list[curr_item_ind]);
+    if (list[curr_item_ind].state == 0) {
+      div.classList.add("completed");
+      checkbox.checked = true;
+      list[curr_item_ind].state = 1;
+      list.activeElements -= 1;
+    } else {
+      div.classList.remove("completed");
+      checkbox.checked = false;
+      list[curr_item_ind].state = 0;
+      list.activeElements += 1;
+    }
+  };
 };
 
 const deleteItem = (id) => {
@@ -71,17 +95,8 @@ const changeState = (id) => {
   const ind = list.findIndex((item) => item.id == id);
 
   const div = document.getElementById(id);
-  if (list[ind].state === 0) {
-    div.classList.add("completed");
-    div.dataset.state = 1;
-    list[ind].state = 1;
-    list.activeElements -= 1;
-  } else {
-    list[ind.state] = 0;
-    div.dataset.state = 0;
-    div.classList.remove("completed");
-    list.activeElements += 1;
-  }
+  console.log(list[ind].state);
+  div.toggleComplete();
 
   update();
 };
@@ -104,6 +119,18 @@ const updateSummaryVisability = () => {
   } else {
     document.getElementsByClassName("summary")[0].classList.remove("hide");
   }
+};
+
+const toggleCompleteButton = () => {
+  let target_val = 1;
+  list.forEach((todo, ind) => {
+    let div = document.getElementById(todo.id);
+    div.toggleComplete(target_val);
+  });
+  target_val = target_val == 1 ? 0 : 1;
+  console.log(target_val);
+
+  update();
 };
 
 const clearCompleted = () => {
